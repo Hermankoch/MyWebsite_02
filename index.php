997d19121c9d64120a2560b873b6c1b62351b50e
Hermankoch <barnelskoch@gmail.com>
2019-12-19 15:23:51 +0200

    Add website

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Web Design Upington</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="fontawesome512/css/all.min.css">
+  </head>
+  <body>
+    <div class="page_background">
+    <nav class="top_nav">
+      <div class="nav_icon">
+      </div>
+      <h2 class="heading_secondary">Creative web design. Web apps. Hosting and more...</h2>
+    </nav>
+    <div class="home_grid">
+    <header class="heading">
+      <h1 class=heading_primary>I help clients bring their bussiness to the online world.</h1>
+    </header>
+    <div class="left_area_grid">
+      <div class="tile">
+        <h3 class="tile_heading">Websites <i class="fas fa-laptop-code"></i></h3>
+        <p class="about-tile-text">Have an idea in mind?</p>
+        <ul class="web_list_wrapper">
+          <li>Create a new website</li>
+          <li>Redesign your website</li>
+          <li>Add an app to your current website</li>
+        </ul>
+      </div>
+
+    <div class="tile">
+      <h3 class="tile_heading">Hosting <i class="fas fa-network-wired"></i></h3>
+      <div class="img_div">
+      <img class="hosting_img"src="webserver.png" alt="server_image"/>
+      </div>
+      <p class="about-tile-text">Get your site online</p>
+      <p class="about-tile-text">From only R49pm</p>
+    </div>
+
+    <div class="tile">
+      <h3 class="tile_heading">About <i class="far fa-address-card"></i></h3>
+      <p class="about-tile-text">Hi, I'm Herman.</p>
+      <p class="about-tile-text">Creative web developer.</p>
+      <p class="about-tile-text u-pad-text">I create custom websites in my free time.</p>
+    </div>
+    <div class="tile">
+      <h3 class="slogan"><span class="red-text">Love</span> at first web<span class="red-text">site</span></h3>
+      <div class="heart_shape"></div>
+    </div>
+  </div> <!-- left grid -->
+  </div> <!-- Home grid -->
+  </div><!-- Page Background -->
+  <div class="page_background">
+  <div class="website_section_title">
+    <h2 class="heading_website_section">My Services</h2>
+  </div>
+  <div class="websites_grid">
+    <div class="web_tile">
+      <h3>Websites</h3>
+    </div>
+    <div class="web_tile">
+      <h3>Web Applications</h3>
+    </div>
+    <div class="web_tile">
+      <h3>Technologies</h3>
+    </div>
+  </div>
+  </div><!-- Page Background -->
+  <div class="websites_grid">
+  </div>
+
+  </body>
+</html>
